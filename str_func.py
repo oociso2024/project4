b3b9a3e7bab5d503b582d8ff356f9765e08ca4c8
Oleg Nanovsky <oociso@ya.ru>
2024-04-28 04:43:52 +0700

    add word_titles

--- str_func.py
+++ str_func.py
@@ -1,5 +1,15 @@
 
 def sts_titles(st):
+    """Функция для преоброзования каждой буквы в слове с заглавной буквы"""
     print(st.upper())
 
 sts_titles("hey")
+
+
+def word_titles(st):
+   """Функция для преоброзования каждого слова с заглавной буквы"""
+   s = st
+   s = s.title()
+   print(s)
+
+word_titles('hello world')
